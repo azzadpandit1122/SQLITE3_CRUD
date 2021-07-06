129504e0e547c47c3102e19bf0a259d1fe765c50
azzadpandit <azzdpandit0@gmail.com>
2021-07-06 23:54:35 +0530

    SQLITE3

--- app/src/main/java/com/example/sqlite3/UsreChargeCollection_Fragment.java
+++ app/src/main/java/com/example/sqlite3/UsreChargeCollection_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.sqlite3;
 
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -8,22 +9,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 
 public class UsreChargeCollection_Fragment extends Fragment {
 
     LineChart chart;
-    private int mfillColor = Color.rgb(150, 181, 229);
+    EditText edittext;
+    final Calendar myCalendar = Calendar.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,13 +40,55 @@ public class UsreChargeCollection_Fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_usre_charge_collection_, container, false);
         chart = view.findViewById(R.id.chart);
+        edittext=view.findViewById(R.id.Birthday);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
         LineDataSet lineDataSet1 = new LineDataSet(dataValues1(), "Data set 1");
+        lineDataSet1.setColor(Color.RED);
+        lineDataSet1.setDrawCircles(false);
+        lineDataSet1.setLineWidth(2);
+        lineDataSet1.setFillAlpha(255);
+        lineDataSet1.setFillColor(Color.BLACK);
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setAxisMaximum(100);
+        leftAxis.setAxisMinimum(0);
+        leftAxis.setDrawAxisLine(false);
+        leftAxis.setDrawZeroLine(false);
+        leftAxis.setDrawGridLines(false);
+
+
         LineDataSet lineDataSet2 = new LineDataSet(dataValues2(), "Data set 2");
+        lineDataSet2.setColor(Color.GREEN);
+
         LineDataSet lineDataSet3 = new LineDataSet(dataValues3(), "Data set 3");
+        lineDataSet3.setColor(Color.BLUE);
+
         LineDataSet lineDataSet4 = new LineDataSet(dataValues4(), "Data set 4");
+        lineDataSet4.setColor(Color.GRAY);
 
         chart.setBackgroundColor(Color.WHITE);
-        chart.setGridBackgroundColor(Color.CYAN);
+       // chart.setGridBackgroundColor(Color.CYAN);
         chart.setDrawGridBackground(true);
         chart.setDrawBorders(true);
         chart.getDescription().setEnabled(false);
@@ -57,19 +108,21 @@ public class UsreChargeCollection_Fragment extends Fragment {
         return view;
     }
 
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+
     private List<Entry> dataValues4() {
         ArrayList<Entry> dataVals = new ArrayList<Entry>();
         dataVals.add(new Entry(0, 23));
         dataVals.add(new Entry(1, 10));
         dataVals.add(new Entry(2, 33));
         dataVals.add(new Entry(3, 66));
-        dataVals.add(new Entry(4, 99));
-        dataVals.add(new Entry(5, 33));
-        dataVals.add(new Entry(6, 99));
-        dataVals.add(new Entry(7, 22));
-        dataVals.add(new Entry(8, 33));
-        dataVals.add(new Entry(9, 56));
-        dataVals.add(new Entry(10, 67));
+        dataVals.add(new Entry(4, 60));
+
         return dataVals;
     }
 
@@ -80,12 +133,7 @@ public class UsreChargeCollection_Fragment extends Fragment {
         dataVals.add(new Entry(2, 56));
         dataVals.add(new Entry(3, 46));
         dataVals.add(new Entry(4, 23));
-        dataVals.add(new Entry(5, 78));
-        dataVals.add(new Entry(6, 32));
-        dataVals.add(new Entry(7, 2));
-        dataVals.add(new Entry(8, 9));
-        dataVals.add(new Entry(9, 0));
-        dataVals.add(new Entry(10, 10));
+
         return dataVals;
     }
 
@@ -96,12 +144,6 @@ public class UsreChargeCollection_Fragment extends Fragment {
         dataVals.add(new Entry(2, 60));
         dataVals.add(new Entry(3, 40));
         dataVals.add(new Entry(4, 70));
-        dataVals.add(new Entry(5, 50));
-        dataVals.add(new Entry(6, 90));
-        dataVals.add(new Entry(7, 40));
-        dataVals.add(new Entry(8, 20));
-        dataVals.add(new Entry(9, 10));
-        dataVals.add(new Entry(10, 30));
         return dataVals;
     }
 
@@ -112,12 +154,6 @@ public class UsreChargeCollection_Fragment extends Fragment {
         dataVals.add(new Entry(2, 30));
         dataVals.add(new Entry(3, 40));
         dataVals.add(new Entry(4, 10));
-        dataVals.add(new Entry(5, 20));
-        dataVals.add(new Entry(6, 40));
-        dataVals.add(new Entry(7, 60));
-        dataVals.add(new Entry(8, 70));
-        dataVals.add(new Entry(9, 10));
-        dataVals.add(new Entry(10, 90));
         return dataVals;
 
     }
